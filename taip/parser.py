12fd2c4de82acff0d9be05f7ffba3fc01fe8d88f
Zhuo Zhang <zhuozhang90@gmail.com>
2019-06-17 12:48:14 -0400

    modify parser

--- taip/parser.py
+++ taip/parser.py
@@ -1,11 +1,12 @@
 def parse(command):
-    if command == "go east":
+    if command == "go east" or command == "e":
         print(">")
-    elif command == "go west":
+    elif command == "go west" or command == "w":
         print("<")
-    elif command == "go north":
+    elif command == "go north" or command == "n":
         print("^")
-    elif command == "go south":
+    elif command == "go south" or command == "s":
         print("v")
+    elif command == "pick up" 
     else:
         print("invalid command")
