a614a1a291c3898c63ffce28610a73ee29e5df75
Zhuo Zhang <zhuozhang90@gmail.com>
2019-07-15 15:01:27 -0400

    test game reader

--- taip/game_reader.py
+++ taip/game_reader.py
@@ -3,9 +3,20 @@ from taip.game_objects import Item, Room, Game
 
 with open("gamefile.json", "r") as game_file:
     data = json.load(game_file)
-    # print(data)
-    for i, j in data["rooms"]["main room"].items():
-        print(i, j)
+    for room, neighbors in data["map"].items():
+        print(room)
+        for _, neighbors in neighbors.items():
+            for direction, room in neighbors.items():
+                print(direction, room)
+    for name, room in data["rooms"].items():
+        print(name)
+        for prop, val in room.items():
+            print(prop)
+            if prop == "children":
+                for i, j in val.items():
+                    print(i, j)
+            else:
+                print(val)
 
 
 
