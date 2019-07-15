6fbcc7b6930fbb2018b2925bc63ce09e3bc5b967
Zhuo Zhang <zhuozhang90@gmail.com>
2019-07-15 13:57:40 -0400

    fix gamefile

--- taip/game_reader.py
+++ taip/game_reader.py
@@ -1,9 +1,9 @@
 import json
-from taip.game_objects import Item, Room, Map
+from taip.game_objects import Item, Room, Game
 
-with open("gamefile.json", "r") as gameFile:
-    data = json.load(gameFile)
-    print(data["start"])
+with open("gamefile.json", "r") as game_file:
+    data = json.load(game_file)
+    print(data)
     print(data["map"]["main room"])
 
 
